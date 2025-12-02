05347467e32137a616d9124b9937bd5b378f77ba
Stormsidee <delay1337pro1@mail.ru>
2025-12-02 14:27:49 +0300

    upd: added Telegram bot in project

--- start.sh
+++ start.sh
@@ -2,15 +2,18 @@
 
 # Активируем виртуальное окружение (если есть)
 if [ -d "venv" ]; then
-    source env/bin/activate
+    source .venv/Scripts/activate
 fi
 
 # Устанавливаем зависимости
 pip install -r requirements.txt
 
+echo "Запуск Telegram Bot"
+nohup python3 Homeassistant1.4.py &
+
 # Запускаем API сервер и Telegram бота в фоне
 echo "Запуск Home Assistant Mini App..."
-python3 api_server.py &
+nohup python3 api_server.py &
 API_PID=$!
 
 echo "API сервер запущен с PID: $API_PID"
